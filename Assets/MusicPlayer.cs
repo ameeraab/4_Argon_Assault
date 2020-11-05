5f76b8e495dfbf93ac4b87e6f930aee62a965477
Ameer AB <ameerasyrafab@gmail.com>
2020-11-05 15:52:40 +0800

    Added music player and added that music to the splash screen. The music player should not be destroyed when the next scene is loaded.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Awake();
+
+        Invoke(nameof(LoadFirstScene), 5f);
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+   private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+}
